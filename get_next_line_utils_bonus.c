44c07e6085ce51581548e3f261183001e887d4e3
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-12-12 17:32:19 +0100

    utils for  bonnus

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 17:04:05 by yoelhaim          #+#    #+#             */
+/*   Updated: 2021/12/12 17:23:02 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (i);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+	int		o_len;
+	int		d_len;
+
+	o_len = ft_strlen(s1);
+	d_len = ft_strlen(s2);
+	new = malloc ((o_len + d_len + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < o_len)
+		new[i++] = s1[j++];
+	j = 0;
+	while (j < d_len)
+		new[i++] = s2[j++];
+	new[i] = '\0';
+	free(s1);
+	return (new);
+}
+
+char	*ft_substr(char *s, int start, int len)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		len = 0;
+	i = 0;
+	j = 0;
+	while (i < len && s[i])
+		i++;
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (j < i)
+		str[j++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
