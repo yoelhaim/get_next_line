67c052df332a4597a695d7d99ef5d163dd460013
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-12-12 17:32:47 +0100

    header next line

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,21 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:52:51 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/12/12 00:48:26 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/12/12 17:21:25 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*ft_substr(char *s, int start, int len);
-char	*ft_strjoin(char  *s1, char  *s2);
-char 	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 int		ft_strlen(char *s);
 
-# endif
+#endif
