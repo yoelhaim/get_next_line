3ff15c0422c135bac175edd0591fab1e5d1793d0
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-12-12 16:31:29 +0100

    main for dubag

---
+++ main.c
@@ -0,0 +1,193 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 13:50:19 by yoelhaim          #+#    #+#             */
+/*   Updated: 2021/12/12 16:30:08 by yoelhaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (i);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+	int		o_len;
+	int		d_len;
+
+	o_len = ft_strlen(s1);
+	d_len = ft_strlen(s2);
+	new = malloc ((o_len + d_len + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < o_len)
+		new[i++] = s1[j++];
+	j = 0;
+	while (j < d_len)
+		new[i++] = s2[j++];
+	new[i] = '\0';
+	free(s1);
+	return (new);
+}
+
+char	*ft_substr(char *s, int start, int len)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		len = 0;
+	i = 0;
+	j = 0;
+	while (i < len && s[i])
+		i++;
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (j < i)
+		str[j++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+
+#ifndef	BUFFER_SIZE
+# define BUFFER_SIZE 42
+
+#endif
+
+char	*str_ret(char *stc_arr)
+{
+	int	i;
+
+	i = 0;
+	if (!stc_arr[i])
+		return (NULL);
+	i = 0;
+	while (stc_arr[i] && stc_arr[i] != '\n')
+		i++;
+	return (ft_substr(stc_arr, 0, i + 1));
+}
+
+char	*str_remainder(char *stc_arr)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!stc_arr)
+		return (NULL);
+	while (stc_arr[i])
+	{
+		if (stc_arr[i] == '\n')
+		{
+			str = ft_substr(stc_arr, i + 1, ft_strlen(stc_arr));
+			free(stc_arr);
+			return (str);
+		}
+		i++;
+	}
+	free (stc_arr);
+	return (NULL);
+}
+
+int	find_nl(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stc_arr;
+	char		*buffer;
+	char		*line;
+	int			i;
+
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	i = 1;
+	buffer = malloc(BUFFER_SIZE + 1);
+	while (!(find_nl(stc_arr)) && (i))
+	{
+		i = read(fd, buffer, BUFFER_SIZE);
+		if (i < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		// printf("%d", i);
+		buffer[i] = '\0';
+		stc_arr = ft_strjoin(stc_arr, buffer);
+	}
+	free(buffer);
+	line = str_ret(stc_arr);
+	stc_arr = str_remainder(stc_arr);
+	return (line);
+}
+
+// int main()
+// {
+// 	int fd = open("hh.txt", O_RDONLY);
+// 	// char *r = get_next_line(fd);
+// 	// int i =0;
+// 	// while (r[i])
+// 	// {
+// 	// 	printf("%c", r[i]);
+
+// 	// }
+// 	printf("%s", get_next_line(fd));
+
+// }
+// int	main(void)
+// {
+// 	int fd = open("hh.txt", O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+
+// }
