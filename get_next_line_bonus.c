c7bda9d09af429e6ea2f44b097c1581d0128df84
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-12-18 12:41:15 +0100

    update  in func find nw  bonnus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 17:04:55 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/12/12 17:22:25 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/12/18 12:40:31 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,13 @@ char	*str_remainder(char *stc_arr)
 
 int	find_nl(char *s)
 {
-	int	i;
-
-	i = 0;
 	if (!s)
 		return (0);
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] == '\n')
+		if (*s == '\n')
 			return (1);
-		i++;
+		s++;
 	}
 	return (0);
 }
